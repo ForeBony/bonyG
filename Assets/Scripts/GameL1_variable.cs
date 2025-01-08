d91ebccb88cc5238abb2e3432cc2714cfb93cee4
ForeBony <pccusatriani@gmail.com>
2025-01-08 21:11:01 +0800

    Initial commit

---
+++ Assets/Scripts/GameL1_variable.cs
@@ -0,0 +1,11 @@
+//單行註解 
+
+// lib
+using UnityEngine;
+
+//class
+public class GameL1_variable: MonoBehaviour
+{
+    public int coin=500;
+    private int level=3;
+}
---
+++ Assets/Scripts/GameL2_dataType.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+/// <summary>
+/// 
+/// </summary>
+public class GameL2_dataType:MonoBehaviour
+{
+    public int count = 7;
+    public float moveSpeed = 3.5;
+    public float turnSpeed = 30.7;
+    public string character = 'Gern';
+    public bool isDead = false;
+    public bool gameOver = true;
+    public byte lv = 16;
+    public uint coin = 9999;
+
+}
